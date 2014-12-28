0d2b586aa665c1252026d0dca22271c1e927e6f2
john <decker49@gmail.com>
2014-12-28 01:26:56 -0600

    Pictures attached to comments now appear. DB name is hard-coded at the moment.

--- views/recent-items/map.js
+++ views/recent-items/map.js
@@ -1,11 +1,18 @@
 function(doc) {
   if (doc.created_at) {
       var p = doc.profile || {};
+      var atts = Object.keys(doc._attachments);
+      var att = null;
+      if( atts ) {
+          att = atts[0];
+      }
       emit(doc.created_at, {
+          id:doc._id,
           message:doc.message,
           gravatar_url : p.gravatar_url,
           nickname : p.nickname,
-          name : doc.name
+          name : doc.name,
+          attachment : att
       });
   }
 };
